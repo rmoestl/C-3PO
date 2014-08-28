0883b30747d79ca06ab4b3c255b9e1eddff4b7f7
Robert Moestl <moestl.robert@gmail.com>
2014-08-28 22:59:03 +0200

    Any source files are now processed by template resolving mechanism. Added some Todos as well.

--- src/main/java/org/c_3po/Main.java
+++ src/main/java/org/c_3po/Main.java
@@ -1,5 +1,6 @@
 package org.c_3po;
 
+import java.io.File;
 import java.io.PrintWriter;
 
 import org.thymeleaf.TemplateEngine;
@@ -11,37 +12,42 @@ public class Main {
 	private static TemplateEngine templateEngine;
 
 	// Configuration Parameters
-	private static String sourceDirectory = "www";
+	private static String sourceDirectoryName = "www";
 	private static String destinationDirectory = "site";
 	
     public static void main(String[] args) throws Exception {
-		processCmdLineArguments(args);
-		System.out.println("Source directory is: " + sourceDirectory);
-		System.out.println("Destination directory is: " + destinationDirectory);
-		
-		// TODO a c-3po.properties file on the classpath
-		
-		TemplateResolver templateResolver = new FileTemplateResolver();
-		templateResolver.setTemplateMode("HTML5");
-		templateResolver.setPrefix(sourceDirectory + "/");
-		templateResolver.setSuffix(".html");
-		templateEngine = new TemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver);
-		
-		Context context = new Context();
-		context.setVariable("welcome", "Very well done, C-3PO!");
-		
-		String result = templateEngine.process("page", context);
-		
+        processCmdLineArguments(args);
         System.out.println("Hello There! I'm C-3PO! Which site do you wish me to generate?");
-		
-		System.out.println("----");
-		System.out.println(result);		
-		System.out.println("----");
-		
-		PrintWriter out = new PrintWriter(destinationDirectory + "/page.html");
-		out.println(result);
-		out.close();
+        System.out.println("Source directory is: " + sourceDirectoryName);
+        System.out.println("Destination directory is: " + destinationDirectory);
+
+        // TODO introduce a partials directory, partials are not copied to destination directory
+        // TODO introduce an img directory, that is copied to the destination directory
+        // TODO extract main functionality here into a separate class so that it can be executed within IntelliJ
+        // TODO a c-3po.properties file on the classpath
+
+        TemplateResolver templateResolver = new FileTemplateResolver();
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setPrefix(sourceDirectoryName + "/");
+        templateResolver.setSuffix(".html");
+        templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("welcome", "Very well done, C-3PO!");
+
+        File sourceDirectory = new File(sourceDirectoryName);
+        if (sourceDirectory.isDirectory()) {
+            for (File file : sourceDirectory.listFiles()) {
+                if (!file.isDirectory()) {
+                    String result = templateEngine.process(file.getName().replace(".html", ""), context);
+
+                    PrintWriter out = new PrintWriter(destinationDirectory + "/" + file.getName());
+                    out.println(result);
+                    out.close();
+                }
+            }
+        }
     }
 	
 	private static void processCmdLineArguments(String[] args) {
@@ -51,7 +57,7 @@ public class Main {
 
 			if ("-src".equals(argument) && i < args.length - 1) {
 				final String sourceDirArgument = args[i + 1];
-				sourceDirectory = sourceDirArgument;
+				sourceDirectoryName = sourceDirArgument;
 				i++;
 			}
 			
