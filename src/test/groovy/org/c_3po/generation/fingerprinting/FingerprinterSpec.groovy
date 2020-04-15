8d78db81acf92e93663a50239b83b80fbd04c8b9
Robert Möstl <moestl.robert@gmail.com>
2020-04-15 15:47:42 +0200

    Test that a css file's outdated fingerprinted version gets replaced by new one

--- src/test/groovy/org/c_3po/generation/fingerprinting/FingerprinterSpec.groovy
+++ src/test/groovy/org/c_3po/generation/fingerprinting/FingerprinterSpec.groovy
@@ -1,12 +1,12 @@
 package org.c_3po.generation.fingerprinting
 
-import org.c_3po.cmd.CmdArguments
-import org.c_3po.generation.SiteGenerator
+
 import spock.lang.Shared
 import spock.lang.Specification
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 
 import static org.c_3po.generation.SiteGenerationHelpers.ensureDestinationDirIsClean
 import static org.c_3po.generation.SiteGenerationHelpers.generateSite
@@ -52,4 +52,19 @@ class FingerprinterSpec extends Specification {
         Files.notExists(cssDir.resolve(
                 "main.6180d1743d1be0d975ed1afbdc3b4c0bfb134124.6180d1743d1be0d975ed1afbdc3b4c0bfb134124.css"))
     }
+
+    def "deletes fingerprinted files if their fingerprint is outdated" () {
+        given: "an already fingerprinted css file"
+        Fingerprinter.fingerprintStylesheets(cssDir, destDir)
+
+        when: "its content is being changed and fingerprinted again"
+        Files.write(destDir.resolve("css/main.css"), ".button { color: blue; }".getBytes(), StandardOpenOption.APPEND)
+        Fingerprinter.fingerprintStylesheets(cssDir, destDir)
+
+        then: "the old fingerprinted version gets replaced by the new fingerprinted one"
+        Files.exists(cssDir.resolve("main.3068d19b5816a4c201a3d72ca4e5e7433537b947.css"))
+        Files.notExists(cssDir.resolve("main.6180d1743d1be0d975ed1afbdc3b4c0bfb134124.css"))
+    }
+
+    // TODO: Revert all fingerprinted files after each feature in a cleanup method
 }
