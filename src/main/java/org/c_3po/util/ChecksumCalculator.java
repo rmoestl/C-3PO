94083157b58f1aaedaaa669ca854605b9e4c74e9
Robert Möstl <moestl.robert@gmail.com>
2020-04-16 14:51:00 +0200

    User buffered input reading in ChecksumCalculator
    - Reason: BufferedInputStream, which is wrapping an input stream,
      takes care of reading the optimal amount of bytes from the
      file system (depends on block size etc.) to avoid slow
      file system operations.

--- src/main/java/org/c_3po/util/ChecksumCalculator.java
+++ src/main/java/org/c_3po/util/ChecksumCalculator.java
@@ -1,5 +1,6 @@
 package org.c_3po.util;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,13 +10,12 @@ import java.security.NoSuchAlgorithmException;
 public class ChecksumCalculator {
     public static byte[] computeSha1Hash(Path file) throws NoSuchAlgorithmException, IOException {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
-        try (var is = Files.newInputStream(file)) {
-            // TODO: Consider increasing buffer size according to https://stackoverflow.com/a/237495/1029261.
+        try (var bis = new BufferedInputStream(Files.newInputStream(file))) {
             byte[] buffer = new byte[1024];
 
             int numBytesRead = 0;
             while (numBytesRead != -1) {
-                numBytesRead = is.read(buffer);
+                numBytesRead = bis.read(buffer);
                 if (numBytesRead > 0) {
 
                     // It's crucial to not just pass `buffer` because then `.update`
